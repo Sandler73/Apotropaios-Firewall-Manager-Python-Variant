53457b271ce7069de9466a0a22caef42d15ed371
Ryan <Sandler73@users.noreply.github.com>
2026-07-17 16:17:43 -0400

    Create init file for unit test package
    Added package marker for unit test package.

---
+++ tests/unit/__init__.py
@@ -0,0 +1,7 @@
+# ==============================================================================
+# File:         tests/unit/__init__.py
+# Project:      Apotropaios - Firewall Manager (Python Variant)
+# Synopsis:     Unit test package marker
+# Description:  Package for isolated unit tests of individual framework functions and classes.
+# Version:      1.6.2
+# ==============================================================================
